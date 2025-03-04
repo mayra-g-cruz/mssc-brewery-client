aa73aaeb1a4c09210135330041e965dee31119ab
mayra-g-cruz <mayragcruzburgos@gmail.com>
2025-03-03 21:09:26 -0600

    client implementation with rest template

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,29 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+
+    @Setter
+    private String apihost;
+
+    private final RestTemplate restTemplate;
+    public final String BEER_PATH_V1 = "/api/v1/beer";
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeer(UUID beerId) {
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + "/" + beerId, BeerDto.class);
+    }
+}
